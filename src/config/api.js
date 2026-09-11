901fed3f67fd17e2355147ab2c03183b36dd90c1
Dennis Jason <jasonden98@gmail.com>
2026-09-11 01:44:38 +0000

    replace backend enpoint with domain

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://52.147.124.25:5000/api/v1"
+    baseURL: "https://be-dennis.studentdumbways.my.id/api/v1"
 });
 
 const setAuthToken = (token) => {
